cefd2abf646914201c5f1b7ee2ea30824d7ecf06
srbelk <srbelk@gmail.com>
2025-02-17 22:16:39 -0500

    added character count

--- main.py
+++ main.py
@@ -2,19 +2,29 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
 
-    print(file_contents)
+    words = file_contents.split()
 
-    words = file_contents.split() 
+    print("--- Begin report of books/frankenstein.txt ---") 
 
     def count_words():
         num = 0
         for word in words:
             num += 1 
-        print(num)
+        print(f"{num} words found in the document")
     count_words()
 
     def count_characters():
+        letters = {}
+        count = 1
         franken_str = file_contents.lower()
-        
-    count_characters()                     
+        for chtr in franken_str:
+            if chtr not in letters:
+                letters[chtr] = count
+            else:
+                letters[chtr] += 1         
+        print(letters)    
+    count_characters()
+    
+    
+                     
 main() 
