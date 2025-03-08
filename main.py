b0e09943f8670d9555f4f7bd1d763a8aea3bd669
srbelk <srbelk@gmail.com>
2025-03-08 11:58:15 -0500

    function to get count of words in txt file and print number to terminal

--- main.py
+++ main.py
@@ -2,9 +2,19 @@ filepath = "books/frankenstein.txt"
 
 def get_book_text(filepath):
     with open(filepath) as f:
-        return f.read()
+        return f.read() 
+
+   
 
 def main():
     text = get_book_text(filepath)
-    print(text)
+
+    def count_words(text):
+        list_of_words = text.split()
+        num_words = 0
+        for word in list_of_words:
+            num_words +=1
+        print(f"{num_words} words found in the document")
+    
+    count_words(text)    
 main()
